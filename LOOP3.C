ea8b4a1d50c67295e56ea2dfe38feccb5d3cd1ac
Aadesh Agrawal <74392722+aadesh-2806@users.noreply.github.com>
2021-05-14 10:45:37 +0530

    Add files via upload

---
+++ LOOP3.C
@@ -0,0 +1,112 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,n,x;
+	
+	printf("Welcome\nEnter a value=\n");
+	scanf("%d" ,&n);
+
+	i=1;
+	while(i<=n)
+	{
+		printf("%d",i);
+		i=i+1;
+	}
+
+	i=i-2;
+	while(i>=1)
+
+	{
+		printf("%d",i);
+		i=i-1;
+	}
+	printf("\n");
+
+	x=n-1;
+	while(x>=1)
+	{
+		i=1;
+
+		while(i<=x)
+		{
+			printf("%d",i);
+			i=i+1;
+		}
+		i=n-x;
+		while(i>=1)
+		{
+			printf(" ");
+			i=i-1;
+		}
+	       i=1;
+		while(i<=n-x-1)
+		{
+			printf(" ");
+			i=i+1;
+		}
+		i=x;
+		while(i>=1)
+		{
+			printf("%d",i);
+			i=i-1;
+		}
+		printf("\n");
+
+		x=x-1;
+
+	}
+	x=2;
+	while(x<=n-1)
+	{
+		i=1;
+
+		while(i<=x)
+		{
+			printf("%d",i);
+			i=i+1;
+		}
+		i=n-x;
+		while(i>=1)
+		{
+			printf(" ");
+			i=i-1;
+		}
+	       i=1;
+		while(i<=n-x-1)
+		{
+			printf(" ");
+			i=i+1;
+		}
+		i=x;
+		while(i>=1)
+		{
+			printf("%d",i);
+			i=i-1;
+		}
+		printf("\n");
+
+		x=x+1;
+
+	}
+	i=1;
+	while(i<=n)
+	{
+		printf("%d",i);
+		i=i+1;
+	}
+
+	i=i-2;
+	while(i>=1)
+
+	{
+		printf("%d",i);
+		i=i-1;
+	}
+	printf("\n");
+
+
+		getch();
+
+
+}
